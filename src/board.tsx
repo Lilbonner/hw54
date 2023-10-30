5fa65dcd0d8f866243a9ecf531a9f99cf2e644d6
Daniyar Nasirov <lilbonnie666@gmail.com>
2023-10-30 18:59:09 +0600

    mod board

---
+++ src/board.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+interface BoardProps {
+  items: boolean[];
+  onCellClick: (index: number) => void;
+  resetGame: () => void;
+}
+
+const Board: React.FC<BoardProps> = ({ items, onCellClick, resetGame }) => {
+  const [reset, setReset] = useState(false);
+
+  const handleResetClick = () => {
+    setReset(true);
+    setTimeout(() => {
+      setReset(false);
+    }, 100);
+    resetGame();
+  };
+
+  return (
+    <div>
+      <div className="board">
+        {items.map((hasItem, index) => (
+          <Cell key={index} hasItem={hasItem} onClick={() => onCellClick(index)} reset={reset} />
+        )}
+      </div>
+      <button className="reset-button" onClick={handleResetClick}>
+        Сброс
+      </button>
+    </div>
+  );
+};
+
+export default Board;
